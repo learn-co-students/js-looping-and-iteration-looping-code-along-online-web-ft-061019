faaa9c6ac8aa5db987454b485dae7b5e59a0c95e
latinawhocodes <alyssaroque01@gmail.com>
2019-10-08 01:21:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let cards = [];
+    for (let i = 0; i < namesArray.length; i++ ) {
+        console.log(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return cards
+}
+
+writeCards(names, 'birthday');
+
+
+function countdown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -=1;
+    }
+    console.log(number);
+}
